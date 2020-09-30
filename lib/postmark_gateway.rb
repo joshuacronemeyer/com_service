26f8a557ba484c2ae5fe72d5a50a387dd91aa74d
Josh Cronemeyer <joshuacronemeyer@gmail.com>
2020-09-30 13:08:46 -0700

    If postmark does not send it we will raise an error. This will be enough for some use cases, but not others. Will get feedback on this approach asap

--- lib/postmark_gateway.rb
+++ lib/postmark_gateway.rb
@@ -7,20 +7,30 @@ class PostmarkGateway
   MAIL_URL = 'https://api.postmarkapp.com/email'
 
   def send_email(email:)
+    req = http_request(email: email)
+    response = http_adapter.request(req)
+    handle_500(response: response)
+    handle_400(response: response)
+  end
+
+  private
+
+  def http_adapter
     uri = URI.parse(MAIL_URL)
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
+    https
+  end
+
+  def http_request(email:)
+    uri = URI.parse(MAIL_URL)
     req = Net::HTTP::Post.new(uri.path)
     req['content-type'] = 'application/json'
     req['accept'] = 'application/json'
     req['X-Postmark-Server-Token'] = ensure_postmark_configuration
-
     req.body = postmark_email_json(email: email)
-    response = https.request(req)
-    handle_500(response: response)
+    req
   end
-
-  private
 
   def postmark_email_json(email:)
     %({
@@ -42,6 +52,12 @@ class PostmarkGateway
 
   def handle_500(response:)
     return unless response.code.to_i >= 500
+
+    raise "Postmark responded with: code-#{response.code} : message-#{response.message} : body-#{response.body}"
+  end
+
+  def handle_400(response:)
+    return unless response.code.to_i >= 400 && response.code.to_i <= 499
 
     raise "Postmark responded with: code-#{response.code} : message-#{response.message} : body-#{response.body}"
   end
--- test/postmark_gateway_test.rb
+++ test/postmark_gateway_test.rb
@@ -15,6 +15,15 @@ class PostmarkGatewayTest < Minitest::Test
     assert_equal 'Postmark responded with: code-500 : message-Internal Server Error : body-', error.message
   end
 
+  def test_postmark_400_raises
+    stub_request(:post, PostmarkGateway::MAIL_URL).to_return(status: [400, 'That email address bounced.'])
+    email = Email.new(to: 'george@example.com', to_name: 'George', from: 'martha@example.com', from_name: 'Martha', subject: "Let's go", body: 'I want to go.')
+    error = assert_raises RuntimeError do
+      PostmarkGateway.new.send_email(email: email)
+    end
+    assert_equal 'Postmark responded with: code-400 : message-That email address bounced. : body-', error.message
+  end
+
   def test_json_to_postmark_contains_all_our_great_data
     stub_request(:post, PostmarkGateway::MAIL_URL)
     email = Email.new(to: 'george@example.com', to_name: 'George', from: 'martha@example.com', from_name: 'Martha', subject: "Let's go", body: 'I want to go.')
